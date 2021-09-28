a2d9e3d18d444ecffa13d8318cdadc9cf547fa62
Ali Mirzaev <mrzv.uz@gmail.com>
2021-09-27 20:32:52 -0500

    Creating classes and functions inside the class

--- oop_in_python.py
+++ oop_in_python.py
@@ -1,9 +1,26 @@
-my_student = {
-    "name": "Rolf Smith",
-    "grades": [70, 88, 90, 99]
-}
+# my_student = {
+#     "name": "Rolf Smith",
+#     "grades": [70, 88, 90, 99]
+# }
 
-def avarage_grade(student):
-    return sum(student["grades"]) / len(student["grades"])
 
-print(avarage_grade(my_student))
+# def avarage_grade(student):
+#     return sum(student["grades"]) / len(student["grades"])
+
+# print(avarage_grade(my_student))
+
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+
+    def avarage(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student_one = Student("Rolf Smith", [70, 88, 90, 99])
+print(student_one.name)
+print(student_one.grades)
+print(student_one.avarage())
