979f06cf3fa33dbc83a73a6517e7bcc0888b3540
Ali Mirzaev <mrzv.uz@gmail.com>
2021-09-27 20:49:42 -0500

    Class action in background.

--- oop_in_python.py
+++ oop_in_python.py
@@ -1,13 +1,13 @@
-# my_student = {
-#     "name": "Rolf Smith",
-#     "grades": [70, 88, 90, 99]
-# }
+my_student = {
+    "name": "Rolf Smith",
+    "grades": [70, 88, 90, 99]
+}
 
 
-# def avarage_grade(student):
-#     return sum(student["grades"]) / len(student["grades"])
+def avarage_grade(student):
+    return sum(student["grades"]) / len(student["grades"])
 
-# print(avarage_grade(my_student))
+print(avarage_grade(my_student))
 
 
 class Student:
@@ -21,6 +21,8 @@ class Student:
 
 
 student_one = Student("Rolf Smith", [70, 88, 90, 99])
+
 print(student_one.name) # student_one becomes an object and name becomes property.
 print(student_one.grades) # student_one becomes an object and grades becomes property.
 print(student_one.avarage()) # avarage becomes a method.
+print(Student.avarage(student_one)) # In the background class Student runs avarage method and passes student_one object inside the brackets.
