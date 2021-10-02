6f30d0bf6c8d45e62b0d0d50127b8bc389bfe033
Ali Mirzaev <mrzv.uz@gmail.com>
2021-10-02 10:43:03 -0500

    classmethod better thatn staticmethod

--- classmethod_staticmethod.py
+++ classmethod_staticmethod.py
@@ -40,7 +40,7 @@ another_object.hi()
 print("--------------------------------------------")
 
 
-# Better examples.
+# Better example with @staticmethod
 
 class FixedFloat:
     def __init__(self, amount):
@@ -49,6 +49,57 @@ class FixedFloat:
     def __repr__(self):     # we have a __repr__(self) repr method which return string representing the object 
         return f"<FixedFloat {self.amount:.2f}>" # .2f is going to print two amount of decimal places. 2.5909 will be 2.59
 
+    @staticmethod
+    def from_sum(value1, value2):
+        return FixedFloat(value1 + value2)
 
-number = FixedFloat(18.8784)
-print(number)
+
+new_number = FixedFloat.from_sum(19.858, 0.789)
+print(new_number)
+
+
+class Euro(FixedFloat):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.symbol = "€"
+
+    def __repr__(self):
+        return f"<Euro {self.symbol}{self.amount:.2f}>"
+
+
+money = Euro(18.786)
+print(money)
+
+
+print("--------------------------------------------")
+
+
+# Better example with @classmethod.
+
+class FixedFloat:
+    def __init__(self, amount):
+        self.amount = amount    # we have __init__ method, in which amount property gets initialized in (self.amount) the object
+
+    def __repr__(self):     # we have a __repr__(self) repr method which return string representing the object 
+        return f"<FixedFloat {self.amount:.2f}>" # .2f is going to print two amount of decimal places. 2.5909 will be 2.59
+
+    @classmethod
+    def from_sum(cls, value1, value2):
+        return cls(value1 + value2)
+
+
+new_number = FixedFloat.from_sum(19.858, 0.789)
+print(new_number)
+
+
+class Euro(FixedFloat):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.symbol = "€"
+
+    def __repr__(self):
+        return f"<Euro {self.symbol}{self.amount:.2f}>"
+
+
+money = Euro.from_sum(18.786, 9.999)
+print(money)
