378206f6e7ff9037ccd3763f2ca1caada130fdfd
Ali Mirzaev <ali@Alis-Air.hsd1.tn.comcast.net>
2021-10-06 05:54:41 -0500

    Updates

--- decorator.py
+++ decorator.py
@@ -1,4 +1,4 @@
-class  Student:
+class Student:
     def __init__(self, name, school):
         self.name = name
         self.school = school
@@ -8,7 +8,7 @@ class  Student:
         return sum(self.marks) / len(self.marks)
 
 
-student_details = Student("Rolf", "Shcool_Name")
+student_details = Student("Rolf", "Schcool_Name")
 
 student_details.marks.append(90)
 student_details.marks.append(85)
@@ -16,14 +16,15 @@ student_details.marks.append(85)
 
 class WorkingStudent(Student):
     def __init__(self, name, school, salary):
-        super().__init__(name, school) # super() is a parent class which is Student
+        super().__init__(name, school)  # super() is a parent class which is Student
         self.salary = salary
-    
-    @property   # property decorator enables weekly_salary to act as an object not as methos. You can call weekly_salary() without brackets.
+
+    # property decorator enables weekly_salary to act as an object not as methos. You can call weekly_salary() without brackets.
+    @property
     def weekly_salary(self):
         return self.salary * 37.5
-    
+
 
 rolf = WorkingStudent("Rolf", "MIT", 25.50)
-# print(rolf.weekly_salary()) 
+# print(rolf.weekly_salary())
 print(rolf.weekly_salary)
