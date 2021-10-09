4dba03ce2eca2f59b3788518e515254489052eca
Ali Mirzaev <mrzv.uz@gmail.com>
2021-10-09 16:43:28 -0500

    Working with errors

---
+++ test_files/errors.py
@@ -0,0 +1,12 @@
+class RunTimeErrorWithCode(TypeError):
+    # This message in multi quotes called dockstring.
+    """
+    Exception raised when a specific error code is needed.
+    """
+
+    def __init__(self, message, code):
+        super().__init__(f"Error code {code}: {message}")
+        self.code = code
+
+
+raise RunTimeErrorWithCode("OUCH! An error happened.", 500)
